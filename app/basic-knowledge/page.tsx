a1be27f35b93ff2e784e5fae1a18bb40c0104eb9
haibinh.phan <haibinh.phan@shopee.com>
2025-06-19 17:19:16 +0700

    Add algorithm's exercises

--- app/basic-knowledge/page.tsx
+++ app/basic-knowledge/page.tsx
@@ -5,12 +5,14 @@ import ContentListPage from "../../components/ContentListPage";
 import advancedJavascriptContent from "../../data/basic-knowledge/advanced-javascript.json";
 import typescriptContent from "../../data/basic-knowledge/typescript.json";
 import algorithmsContent from "../../data/basic-knowledge/algorithms.json";
+import algorithmExercisesContent from "../../data/basic-knowledge/algorithm-exercises.json";
 
 // Define content for basic knowledge
 const contentMap = {
   "advanced-javascript": advancedJavascriptContent,
   typescript: typescriptContent,
   algorithms: algorithmsContent,
+  "algorithm-exercises": algorithmExercisesContent,
 };
 
 export default function BasicKnowledgePage() {
